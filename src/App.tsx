67cdad7d6a5524b22d146d6e7896f5fea2a9a49a
현성 김 <hs-kim@mantech.co.kr>
2023-01-04 22:47:03 +0900

    framer motion Gestures

--- src/App.tsx
+++ src/App.tsx
@@ -12,20 +12,60 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 10px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 30px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVars = {
-  start: { scale: 0 },
-  end: { rotateZ: 360, scale: 1, transition: { type: 'spring', delay: 0.5 } },
+const Circle = styled(motion.div)`
+  background-color: white;
+  height: 70px;
+  width: 70px;
+  place-self: center;
+  border-radius: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariant = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: 'spring',
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
 };
 
 function App(): JSX.Element {
   return (
     <Wrapper>
-      <Box variants={myVars} initial='start' animate='end' />
+      <Box variants={boxVariant} initial='start' animate='end'>
+        <Circle variants={circleVariants}></Circle>
+        <Circle variants={circleVariants}></Circle>
+        <Circle variants={circleVariants}></Circle>
+        <Circle variants={circleVariants}></Circle>
+      </Box>
     </Wrapper>
   );
 }
