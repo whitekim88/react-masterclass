85d09ea3e946cd6998523d6f96b2f129612e9e18
현성 김 <hs-kim@mantech.co.kr>
2023-01-04 22:24:45 +0900

    framer motion animation,variant

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -8,7 +9,7 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
@@ -16,10 +17,15 @@ const Box = styled.div`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const myVars = {
+  start: { scale: 0 },
+  end: { rotateZ: 360, scale: 1, transition: { type: 'spring', delay: 0.5 } },
+};
+
 function App(): JSX.Element {
   return (
     <Wrapper>
-      <Box></Box>
+      <Box variants={myVars} initial='start' animate='end' />
     </Wrapper>
   );
 }
